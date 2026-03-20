bd9b0080de244e880b5b58867a2a76d8aabcd13c
MoonShinerYe <maradonaegor@mail.ru>
2026-03-21 02:03:48 +0700

    HW3 UI

---
+++ iosEltex/iosEltex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosEltex
+//
+//  Created by Егор Колесников on 21.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
